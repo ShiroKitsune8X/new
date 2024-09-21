e6ccf884f33f67260f0010a20798a439c3024130
ShiroKitsune8X <154643144+ShiroKitsune8X@users.noreply.github.com>
2024-09-21 06:25:08 +0300

    Add files via upload

---
+++ задание пермитра и плошади квадрата.cpp
@@ -0,0 +1,34 @@
+﻿// задание пермитра и плошади квадрата.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+void kva(float side,float& perimeter,float& square){                // обьявление функцие
+    perimeter = 4 * side;
+    square = side * side;
+}
+
+int main()
+{
+    float side;                                                     //присвоение вывода
+    cout << "enter the data" << endl;
+    cin >> side;
+    float square, perimeter;
+    kva(side, perimeter, square);
+    cout << " perimeter kva " << perimeter << endl;
+    cout << " square kva " << square << endl;
+   (perimeter > square) ? cout << " perimeter " : cout << "squre";       // выполняется проверка 
+    return 0;
+}
+
+           /*   if (perimeter > square) {                               // выполняется проверка 
+    cout << "perimeter more" << endl;
+
+    }
+    else if (square > perimeter) {
+    cout << "square more" << endl;
+    }
+    else {
+    cout << "=" << endl;
+    } */  
---
+++ найти выражение.cpp
@@ -0,0 +1,23 @@
+﻿// найти выражение.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int take(int x) {
+    x = pow(x,3) - pow(x,2) + x - 1;                // функция задана
+    return x;
+ }
+
+int main()
+{
+    int a, b, c, z;                         // обьявление переменых
+   cout << "enter data" << endl;
+   cin >> a >> b >> c;
+  z= (take(2 * a) + take(b + c));           //решение  выражение 
+   cout << " result " << z << endl;
+   return 0;
+
+    
+}
+
